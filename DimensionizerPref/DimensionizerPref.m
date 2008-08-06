4a59ee86efdb3546950b37332ba3c48b70637e04
Travis Cripps <travis@trav.is>
2008-08-06 06:28:29 +0000

    Fixed some issues with registering and unregistering to observe the shared user defaults controller's 'values' by rearranging the code to better utilize the appropriate delegate callbacks.\n\nAdded a lot of documentation for the functions.

--- DimensionizerPref/DimensionizerPref.m
+++ DimensionizerPref/DimensionizerPref.m
@@ -21,7 +21,11 @@
     [DimensionizerPref setupDefaults];
 }
 
-
+/*!
+ @function
+ @abstract   Sets up the User Defaults dictionary with the default values it should expect.
+ @discussion The user defaults controller will only write the values that are different from these default values to the user defaults database.
+ */
 + (void)setupDefaults {
     NSDictionary *_defaultPreferences = [NSDictionary dictionaryWithObjectsAndKeys:
                                          [NSNumber numberWithInt: HTML], PRIMARY_OUTPUT_FORMAT_KEY,
@@ -31,13 +35,15 @@
                                          nil];
     
     [[NSUserDefaults standardUserDefaults] registerDefaults: _defaultPreferences];
-    
-    // Set the initial values in the shared user defaults controller
-    //[[NSUserDefaultsController sharedUserDefaultsController] setInitialValues: _defaultPreferences];
 }
 
-
-- (void) willSelect {
+/*!
+ @function
+ @abstract   Callback function that is executed when the main view is set up and prepared to be displayed.
+ @discussion We use this as a hook to set up the tokens and tokenizing.
+ */
+- (void) mainViewDidLoad {
+	//NSLog(@"Main view did load.");
 	NSCharacterSet *set = [NSCharacterSet characterSetWithCharactersInString: @""];
     
 	[primaryCustomFormatField setTokenizingCharacterSet: set];
@@ -65,41 +71,75 @@
     [predefinedWidthField2 setObjectValue: [NSArray arrayWithObject: WIDTH_TOKEN]];
 }
 
+/*!
+ @function
+ @abstract   Registers this controller as an observer of the "values" key of the shared user defaults controller.
+ @discussion We do this so we will get notifications when the user defaults values change.
+ */
+- (void) registerAsObserver {
+	//NSLog(@"Registering as an observer to 'values'.");
+	[[NSUserDefaultsController sharedUserDefaultsController] addObserver: self
+															  forKeyPath: @"values"
+																 options: (NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld)
+																 context: NULL];
+}
 
-- (void) mainViewDidLoad {
+/*!
+ @function
+ @abstract   Callback function that is executed when the main application is about to display the preference pane’s main view.
+ @discussion 
+ */
+- (void) willSelect {
+	//NSLog(@"Will select.");
+	
+}
+
+/*!
+ @function
+ @abstract   Notification callback function received when the main application has just displayed the preference pane’s main view.
+ @discussion We're using it to register as an observer to the shared user defaults object.
+ */
+- (void) didSelect {
+	//NSLog(@"Did select.");
     [self registerAsObserver];
 }
 
-- (void) registerAsObserver {
-	NSLog(@"Registering as an observer to 'values'.");
-	@try {
-		[[NSUserDefaultsController sharedUserDefaultsController] addObserver: self
-																  forKeyPath: @"values"
-																	 options: (NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld)
-																	 context: NULL];
-	} 
-	@catch (id theException) {
-		NSLog(@"Couldn't unregister as an observer to 'values': %@", theException);
-	}
-}
-
+/*!
+ @function
+ @abstract   Notification callback function received when the main application has just stopped displaying the preference pane’s main view.
+ @discussion We're using it to unregister as an observer to the shared user defaults object.
+ */
 - (void) didUnselect {
+	//NSLog(@"Did unselect.");
     [self unregisterForChangeNotification];
 }
 
 - (void) unregisterForChangeNotification {
-	NSLog(@"UN-registering as an observer to 'values'.");
-    [[NSUserDefaultsController sharedUserDefaultsController] removeObserver: self forKeyPath:@"values"];
+	//NSLog(@"UN-registering as an observer to 'values'.");
+	@try {
+		[[NSUserDefaultsController sharedUserDefaultsController] removeObserver: self forKeyPath:@"values"];
+	}
+	@catch (NSException * e) {
+		NSLog(@"Couldn't unregister as an observer to 'values': %@", e);
+	}
 }
 
-
+/*!
+ @function
+ @abstract   Notification callback function received when the value of the observed object changes.
+ @discussion In this case, we're using it to know when the user defaults values have changed, so we can synchronize the user defaults.
+ @param      keyPath to observe
+ @param      object whose keyPath to observe
+ @param      change dictionary
+ @param      context of the changes
+ */
 - (void) observeValueForKeyPath: (NSString *)keyPath
                        ofObject: (id)object
                          change: (NSDictionary *)change
                         context: (void *)context
 {
-    NSLog(@"observeValueForKeyPath called");
-    if ([keyPath isEqual: @"values"]) {
+    //NSLog(@"observeValueForKeyPath called");
+    if ([keyPath isEqualToString: @"values"]) {
         [[[NSUserDefaultsController sharedUserDefaultsController] defaults] synchronize];
     }
     
@@ -135,7 +175,14 @@
 }
 */
 
-
+/*!
+    @function
+    @abstract   NSTokenField delegate method that determines the display string for a token.
+    @discussion 
+    @param      tokenField that is invoking the method
+	@param      representedObject the token to represent
+    @result     the string that should be displayed for the token
+*/
 - (NSString *) tokenField: (NSTokenField *)tokenField displayStringForRepresentedObject: (id)representedObject {
 	//NSLog(@"displayStringForRepresentedObject");
 	NSString *string;
@@ -152,6 +199,14 @@
 	return string;
 }
 
+/*!
+ @function
+ @abstract   NSTokenField delegate method that determines the token style for a token.
+ @discussion 
+ @param      tokenField that is invoking the method
+ @param      representedObject the token to represent
+ @result     the token style that should be displayed for the token
+ */
 - (NSTokenStyle) tokenField: (NSTokenField *)tokenField styleForRepresentedObject: (id)representedObject {
 	//NSLog(@"styleForRepresentedObject: Field %@ with value: %@", tokenField, representedObject);
     if ([(NSString *)representedObject isEqualToString: HEIGHT_TOKEN] || 
@@ -162,9 +217,17 @@
 	return NSPlainTextTokenStyle;
 }
 
-
+/*!
+ @function
+ @abstract   NSTokenField delegate method that places a representation of the tokens onto the pasteboard.
+ @discussion We use this method to serialize the tokens into a format that we can parse.
+ @param      tokenField that is invoking the method
+ @param      objects to write to the pasteboard
+ @param		 pboard to which the objects' representation should be written
+ @result     the result of putting the representation of the objects onto the pasteboard
+ */
 - (BOOL) tokenField: (NSTokenField *)tokenField writeRepresentedObjects: (NSArray *)objects toPasteboard: (NSPasteboard *)pboard {
-    NSLog(@"writeRepresentedObjectstoPasteboard: %@", pboard);
+    //NSLog(@"writeRepresentedObjectstoPasteboard: %@", pboard);
     BOOL result = YES;
     
     NSMutableString *string = [NSMutableString stringWithCapacity: 100];
@@ -186,6 +249,14 @@
     return result;
 }
 
+/*!
+ @function
+ @abstract   NSTokenField delegate method that reads the pasteboard's representation of tokens and deserializes it into real tokens.
+ @discussion We use this method to unserialize the tokens from the format we serialized them to with the 'writeRepresentedObjects'... function.
+ @param      tokenField that is invoking the method
+ @param		 pboard to which the objects' representation should be written
+ @result     the array of tokens from the pasteboard representation
+ */
 - (NSArray *) tokenField: (NSTokenField *)tokenField readFromPasteboard: (NSPasteboard *)pboard {
     NSMutableArray *result = [NSMutableArray array];
     
